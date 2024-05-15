b13d6cd9b7dda2bbb70485e21dfbe54a3bade478
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2024-05-15 15:30:05 +0300

    setup realtime leaderboard with rank status

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>
-      <h1>Live Dashboard</h1>
-      <ul>
-        <li v-for="user in users" :key="user.user_id">
-          User ID: {{ user.user_id }}, Max user: {{ user.max_score }}
-        </li>
-      </ul>
+    <div class="leaderboard">
+      <h2>Real-Time Leaderboard</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Score</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.user_id">
+            <td>{{ user.rank+1 }}</td>
+            <td>{{ user.username }}</td>
+            <td>{{ user.max_score }}</td>
+            <td>
+              <span v-if="user.change < 0" class="arrow up">↑</span>
+              <span v-else-if="user.change > 0" class="arrow down">↓</span>
+              <span v-else class="arrow stop">-</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  </template>
+</template>
 <script setup>
 import { io } from "socket.io-client";
 import { ref, onMounted, onUnmounted } from 'vue';
@@ -22,8 +39,22 @@ const connectToSocket = () => {
     // Connect to the server; replace 'http://localhost:3000' with your server's URL if different
     socket.emit('score request')
 
-    socket.on('score update', (newUsers) => {
-        users.value = newUsers;
+    socket.on('score update', (updatedUsers) => {
+        if(users.value.length === 0){
+            users.value = updatedUsers.map((newUser, index) => ({...newUser, rank: index,change:0}));
+        }
+        else{
+            // Process incoming data to calculate changes
+            const newUsers = updatedUsers.map((newUser,index) => {
+            const existing = users.value.find(user => user.user_id === newUser.user_id);
+            return {
+                ...newUser,
+                rank: index,
+                change: existing ? index - existing.rank : 0
+            };
+            });
+            users.value = newUsers;
+        }
     });
 
     socket.on('connect_error', (error) => {
@@ -37,8 +68,25 @@ onUnmounted(()=> {
     }
 })
 </script>
-  
-  <style>
-  /* Add styles if needed */
-  </style>
+<style>
+table {
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 8px;
+  border-bottom: 1px solid #ccc;
+}
+.arrow.up {
+  color: green;
+}
+.arrow.down {
+  color: red;
+}
+.arrow.stop {
+  color: grey;
+}
+</style>
+
   
